3c633c1ccb607a3eccf4555efdb9456231917235
David Tsai <a0922810725@gmail.com>
2019-05-25 11:47:43 +0800

    Initial commit

---
+++ src/main/kotlin/com/a0922/Hello.kt
@@ -0,0 +1,13 @@
+package com.a0922
+
+fun main(args: Array<String>) {
+    //println("Hellp kotlin")
+    //Human().hello()
+    val h = Human()
+    h.hello()
+}
+class Human {
+    fun hello(){
+        println("hello kotlin")
+    }
+}
