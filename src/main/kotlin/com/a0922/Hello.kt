350c3debaf354bf2ac1cf6706ce91b78dbc0daff
a0922 <a0922810725@gmail.com>
2019-05-26 10:48:06 +0800

    kotlin varible,data type

--- src/main/kotlin/com/a0922/Hello.kt
+++ src/main/kotlin/com/a0922/Hello.kt
@@ -5,6 +5,12 @@ fun main(args: Array<String>) {
     //Human().hello()
     val h = Human()
     h.hello()
+
+    var  age = 19
+    age.plus(1)
+    val weight = 65.5
+    val enroll = true
+    println(age)
 }
 class Human {
     fun hello(){
